c4f74b6a0c3c23133a3a26d3883f15b5fcd49d12
Moralist Festus <46865149+MoralistFestus@users.noreply.github.com>
2020-06-30 14:38:11 +0100

    Js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+let quotes=[
+'The secret to life is to love who you are.',
+'Look for opportunities in every change in your life.',
+'Persist while others are quitting. - <b>Unknown</b>',
+'The moment you stop trying that is the moment you stop achieving. - <b>Moralist Festus.</b>', 
+'Keep trying, Success don\'t come easily, if you want something easy then choose failure because it is very easy to achieve. - <b>Moralist Festus</b>', 
+'Failing today doesn\'t mean you\'ll fail tomorrow, everything is worth putting your best at. - <b>Moralist Festus</b>',
+'Debugging is the acts of remove error or bugs from a program, Then programming is the act of creating those bugs. - <b>Unknown</b>', 
+'Life is the most difficult exam. Many people fail because they try to copy others, not realizing that everyone has a different question paper. - <b>Anonymous</b>',
 
+
+];
+    function displayQuote(){
+      let index=Math.floor(Math.random()*quotes.length);
+     //display the quote of that index and trigger action with event listener
+        let div=document.querySelector('#quote');
+        let quote=`<div class="card">
+       <img src="icon/icon.png">
+       <p>${quotes[index]}</p>
+       <img src="icon/icon.png">
+       </div>`;
+       div.innerHTML=quote;
+     }
+    let btn=document.querySelector('.btn');
+      btn.addEventListener('click', displayQuote);
+      console.log(quotes.length) 
